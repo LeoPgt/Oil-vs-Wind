e81ee648fd3604a88eba120250e28f95cd1cd996
MuMartine <115325177+MuMartine@users.noreply.github.com>
2023-03-21 15:14:55 +0100

    Add files via upload

---
+++ OIL_VS_WIND/src/main/java/com/mycompany/oil_vs_wind/OIL_VS_WIND.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.oil_vs_wind;
+
+/**
+ *
+ * @author bmartine
+ */
+public class OIL_VS_WIND {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
