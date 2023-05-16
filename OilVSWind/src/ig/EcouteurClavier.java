979aecaf3307a083c4cdca2d86dccdd7cc038432
TotoMch <128495125+TotoMch@users.noreply.github.com>
2023-05-16 15:29:20 +0200

    ig essai
    essai fonctionnement ig simple

--- OilVSWind/src/ig/EcouteurClavier.java
+++ OilVSWind/src/ig/EcouteurClavier.java
@@ -14,55 +14,89 @@ import java.awt.Graphics;
  * @author tmichel1
  */
 public class EcouteurClavier implements KeyListener {
- private int x = 350; // coordonnée x du dessin
- private int y = 150; // coordonnée y du dessin
-   
-
     
-    public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        switch (keyCode) {
-        case KeyEvent.VK_UP:
-            y -= 10; // déplace le dessin vers le haut
-            break;
-        case KeyEvent.VK_DOWN:
-            y += 10; // déplace le dessin vers le bas
-            break;
-        case KeyEvent.VK_LEFT:
-            x -= 10; // déplace le dessin vers la gauche
-            break;
-        case KeyEvent.VK_RIGHT:
-            x += 10; // déplace le dessin vers la droite
-            break;
+     private Jeu jeu;
+
+    public EcouteurClavier(Jeu jeu) {
+        this.jeu = jeu;
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent evt) {
+        if (evt.getKeyCode() == evt.VK_RIGHT) {
+            this.jeu.joueur.setDroite(true);
         }
-     
+        if (evt.getKeyCode() == evt.VK_LEFT) {
+            this.jeu.joueur.setGauche(true);
+        }
     }
 
-    public void keyReleased(KeyEvent e) {
-        // Ne fait rien
+    @Override
+    public void keyReleased(KeyEvent evt) {
+        if (evt.getKeyCode() == evt.VK_RIGHT) {
+            this.jeu.joueur.setDroite(false);
+        }
+        if (evt.getKeyCode() == evt.VK_LEFT) {
+            this.jeu.joueur.setGauche(false);
+        }
     }
-
-    public void keyTyped(KeyEvent e) {
-        // Ne fait rien
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
     
+
+    @Override
+    public void keyTyped(KeyEvent event) {
+        //Nothing to do here !
+    }
+    
+    
+// private int x = 350; // coordonnée x du dessin
+// private int y = 150; // coordonnée y du dessin
+//   
+//
+//    
+//    public void keyPressed(KeyEvent e) {
+//        int keyCode = e.getKeyCode();
+//        switch (keyCode) {
+//        case KeyEvent.VK_UP:
+//            y -= 10; // déplace le dessin vers le haut
+//            break;
+//        case KeyEvent.VK_DOWN:
+//            y += 10; // déplace le dessin vers le bas
+//            break;
+//        case KeyEvent.VK_LEFT:
+//            x -= 10; // déplace le dessin vers la gauche
+//            break;
+//        case KeyEvent.VK_RIGHT:
+//            x += 10; // déplace le dessin vers la droite
+//            break;
+//        }
+//     
+//    }
+//
+//    public void keyReleased(KeyEvent e) {
+//        // Ne fait rien
+//    }
+//
+//    public void keyTyped(KeyEvent e) {
+//        // Ne fait rien
+//    }
+//
+//    public int getX() {
+//        return x;
+//    }
+//
+//    public int getY() {
+//        return y;
+//    }
+//
+//    public void setX(int x) {
+//        this.x = x;
+//    }
+//
+//    public void setY(int y) {
+//        this.y = y;
+//    }
+//
+//    
    
     }
     
