75687f4e16a0d0ecdc474293c1c2d49d79b91865
MuMartine <binjaminmartine@gmail.com>
2023-05-16 17:57:42 +0200

    test gzetteur SQL

--- OilVSWind/src/sql/Select_OVSW.java
+++ OilVSWind/src/sql/Select_OVSW.java
@@ -39,4 +39,95 @@ public class Select_OVSW {
         }
 
     }
-}
+     
+ public void get_jouable_x (String[] args) {
+     
+      try {
+
+            Connection connexion = DriverManager.getConnection("jdbc:mariadb://nemrod.ens2m.fr:3306/2022-2023_s2_vs1_tp1_OilvSWind", "etudiant", "YTDTvj9TR3CDYCmP");
+
+         PreparedStatement requete = connexion.prepareStatement("SELECT Coordonée_X FROM Jouable ");
+            System.out.println(requete);
+            ResultSet resultat = requete.executeQuery();
+            while (resultat.next()) {
+                int Coordonée_X  = resultat.getInt("Coordonée_X ");
+                
+                
+                System.out.println( Coordonée_X    );
+            }
+
+            requete.close();
+            connexion.close();
+        } catch (SQLException ex) {
+          
+            
+            ex.printStackTrace();
+        }
+     
+ }
+ 
+  public void get_jouable_y (String[] args) {
+     
+      try {
+
+            Connection connexion = DriverManager.getConnection("jdbc:mariadb://nemrod.ens2m.fr:3306/2022-2023_s2_vs1_tp1_OilvSWind", "etudiant", "YTDTvj9TR3CDYCmP");
+
+         PreparedStatement requete = connexion.prepareStatement("SELECT Coordonée_Y FROM Jouable ");
+            System.out.println(requete);
+            ResultSet resultat = requete.executeQuery();
+            while (resultat.next()) {
+                int Coordonée_Y = resultat.getInt("Coordonée_Y ");
+                
+                
+                System.out.println( Coordonée_Y    );
+            }
+
+            requete.close();
+            connexion.close();
+        } catch (SQLException ex) {
+          
+            
+            ex.printStackTrace();
+        }
+     
+ }
+ 
+ 
+ 
+  public void get_jouable_xy (String[] args) {
+     
+      try {
+
+            Connection connexion = DriverManager.getConnection("jdbc:mariadb://nemrod.ens2m.fr:3306/2022-2023_s2_vs1_tp1_OilvSWind", "etudiant", "YTDTvj9TR3CDYCmP");
+
+         PreparedStatement requete = connexion.prepareStatement("SELECT Coordonée_X, Coordonée_Y FROM Jouable ");
+            System.out.println(requete);
+            ResultSet resultat = requete.executeQuery();
+            while (resultat.next()) {
+                int Coordonée_X  = resultat.getInt("Coordonée_X ");
+                int Coordonée_Y  = resultat.getInt("Coordonée_Y ");
+                
+                System.out.println( Coordonée_X  + "," + Coordonée_Y  );
+            }
+
+            requete.close();
+            connexion.close();
+        } catch (SQLException ex) {
+          
+            
+            ex.printStackTrace();
+        }
+     
+ }
+ 
+ }    
+     
+     
+     
+     
+     
+     
+     
+
+
+
