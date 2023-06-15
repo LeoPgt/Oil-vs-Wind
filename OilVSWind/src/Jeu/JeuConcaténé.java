45d8c5ad166445bcfb751abe31c18037fb9ceacb
RomainMorel117 <128494184+RomainMorel117@users.noreply.github.com>
2023-06-15 08:52:56 +0200

    [...]

--- OilVSWind/src/Jeu/JeuConcaténé.java
+++ OilVSWind/src/Jeu/JeuConcaténé.java
@@ -55,14 +55,14 @@ public class JeuConcaténé {
         this.MapTitle = MapTitle;
         this.C = new Carte(this.MapTitle.getMAP_DATA());
 //        int carteSize = C.getSize();   // pas encore vu l'utilité
-        this.runner = new Runner(0,0,1);
+        this.runner = new Runner(4,"JoueurCours",0,0,1);
         //BARIL
         this.barrilJoueur = new ArrayList<Baril>();
-        Baril B1 = new Baril(3, 1, 1);
+        Baril B1 = new Baril(0,"Joueur1",3, 1, 1,false);
         this.barrilJoueur.add(B1);
-        Baril B2 = new Baril(4, 2, 2);
+        Baril B2 = new Baril(1,"Joueur2",4, 2, 2,false);
         this.barrilJoueur.add(B2);
-        Baril B3 = new Baril(5, 3, 3);
+        Baril B3 = new Baril(2,"Joueur3",5, 3, 3,false);
         this.barrilJoueur.add(B3);
         //this.barrilJoueur.indexOf(B1); //Retourne indice de B1 donc 0
         //this.barrilJoueur.get(0); //retourne l'objet d'indice 0 donc B1
@@ -74,7 +74,7 @@ public class JeuConcaténé {
     public void partie(){
         Carte Map = this.C; // A regarder car il y a PEUT ETRE de nouveaux paramètres dans la fonction
         Regles règle = new Regles(2,this.C);
-        Jouable Joueur = new Jouable(0,1,1);// id, x , y
+        Jouable Joueur = new Jouable("Skylze",0,1,1);// id, x , y
         SwingUtilities.invokeLater(() -> {
         JFrame frame = new JFrame("OIL VS WIND");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
